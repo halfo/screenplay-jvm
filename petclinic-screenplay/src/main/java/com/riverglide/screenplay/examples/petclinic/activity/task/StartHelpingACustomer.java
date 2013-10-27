f1cbd5c5eadb20a682c17731f6f2ff477dd58776
antonymarcano <antony@antonymarcano.com>
2013-10-27 15:04:21 +0000

    Optimised imports

--- petclinic-screenplay/src/main/java/com/riverglide/screenplay/examples/petclinic/activity/task/StartHelpingACustomer.java
+++ petclinic-screenplay/src/main/java/com/riverglide/screenplay/examples/petclinic/activity/task/StartHelpingACustomer.java
@@ -1,8 +1,8 @@
 package com.riverglide.screenplay.examples.petclinic.activity.task;
 
 import com.riverglide.screenplay.Actor;
-import com.riverglide.screenplay.action.Go;
 import com.riverglide.screenplay.Perform;
+import com.riverglide.screenplay.action.Go;
 import com.riverglide.screenplay.examples.petclinic.screen.Home;
 
 public class StartHelpingACustomer implements Perform {
